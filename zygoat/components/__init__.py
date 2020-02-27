0146667c3f1082874c97366b9a75f4636a67a127
Christine Viveiros <christine@willing.com>
2020-02-27 09:41:28 -0800

    Refactor file components, add in pre-commit config

--- zygoat/components/__init__.py
+++ zygoat/components/__init__.py
@@ -1,13 +1,16 @@
 from .base import Component  # noqa
+from .file_component import FileComponent # noqa
 from .settings_component import SettingsComponent  # noqa
 
 from .editorconfig import editorconfig
+from .precommit import precommitconfig
 from .docker_compose import docker_compose
 from .backend import backend
 from .frontend import frontend
 
 components = [
     editorconfig,
+    precommitconfig,
     docker_compose,
     backend,
     frontend,
