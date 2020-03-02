25da1cc4a9fb84df9d080212f797329ddfe7c727
Ian MacLeod <ian.a.macleod@gmail.com>
2020-03-02 11:53:54 -0800

    Add cookie configuration to the Django settings

---
+++ zygoat/components/backend/settings/cookies.py
@@ -0,0 +1,47 @@
+import logging
+
+from zygoat.components import SettingsComponent
+
+log = logging.getLogger()
+
+
+secure_cookie_string = """\
+if not DEBUG:
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+"""
+
+
+class Cookies(SettingsComponent):
+    def create(self):
+        red = self.parse()
+
+        log.info('Adding cookie configuration')
+
+        red.extend([
+            '\n',
+            '\n',
+            '# Cookies',
+            '\n',
+            "SHARED_DOMAIN = env.str('DJANGO_SHARED_DOMAIN', default=None)",
+            '\n',
+            'CSRF_COOKIE_DOMAIN = SHARED_DOMAIN',
+            '\n',
+            'SESSION_COOKIE_DOMAIN = SHARED_DOMAIN',
+            '\n',
+            "SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'",
+            '\n',
+            'SESSION_COOKIE_AGE = 604800  # One week in seconds',
+            '\n',
+            secure_cookie_string,
+        ])
+
+        self.dump(red)
+
+    @property
+    def installed(self):
+        red = self.parse()
+        return red.find('name', value='SESSION_ENGINE') is not None
+
+
+cookies = Cookies()
