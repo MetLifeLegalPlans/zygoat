f31dcd1cd4ecde14f46996b4351bfae2ceb09d48
Mark Rawls <markrawls96@gmail.com>
2020-03-03 19:07:29 -0500

    Add Black python formatter to generated apps

---
+++ zygoat/components/backend/black.py
@@ -0,0 +1,13 @@
+from zygoat.components import FileComponent
+from zygoat.constants import Projects
+
+from . import resources
+
+
+class Black(FileComponent):
+    file_name = 'pyproject.toml'
+    resource_pkg = resources
+    base_path = Projects.BACKEND
+
+
+black = Black()
