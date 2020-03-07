f838cff3e8ba07c35b4759ebab6269632842484a
Christine Viveiros <christine@willing.com>
2020-03-06 20:41:08 -0800

    Add swp files to gitignores

--- zygoat/components/backend/gitignore.py
+++ zygoat/components/backend/gitignore.py
@@ -17,6 +17,10 @@ class GitIgnore(Component):
             res.raise_for_status()
 
             f.write(res.text)
+            f.write("\n# IntelliJ editors\n.idea/")
+            # End with a newline since js-specific stuff will go into the
+            # file next.
+            f.write("\n# Swap files\n*.swp\n")
 
     @property
     def installed(self):
