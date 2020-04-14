61a16094b618f88806e9fc2504dd333aafe88f61
Ian MacLeod <ian.a.macleod@gmail.com>
2020-04-13 17:45:49 -0700

    Allow next config to be extended

--- zygoat/components/frontend/next_config.py
+++ zygoat/components/frontend/next_config.py
@@ -1,14 +1,25 @@
-from zygoat.components import FileComponent
+from zygoat.components import FileComponent, Component
 from zygoat.constants import Projects
 
 from . import resources
 
 
-class NextConfig(FileComponent):
+class NextConfigFile(FileComponent):
     resource_pkg = resources
     base_path = Projects.FRONTEND
     filename = "next.config.js"
     overwrite = False
 
 
-next_config = NextConfig()
+class ZygoatNextConfigFile(FileComponent):
+    resource_pkg = resources
+    base_path = Projects.FRONTEND
+    filename = "zygoat.next.config.js"
+    overwrite = True
+
+
+class NextConfig(Component):
+    pass
+
+
+next_config = NextConfig(sub_components=[NextConfigFile(), ZygoatNextConfigFile()])
