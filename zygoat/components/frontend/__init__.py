1ff5dd5682ffd3dde6a9f9c96458e5525be1cf0f
Ian MacLeod <ian.a.macleod@gmail.com>
2020-07-29 18:06:04 -0700

    NEXT: remove -e flag from create-next-app command

--- zygoat/components/frontend/__init__.py
+++ zygoat/components/frontend/__init__.py
@@ -24,7 +24,7 @@ class Frontend(Component):
         run(["npm", "install", "-g", "--upgrade", "yarn"])
 
         log.info("Running create-next-app")
-        run(["yarn", "create", "next-app", Projects.FRONTEND, "-e"])
+        run(["yarn", "create", "next-app", Projects.FRONTEND])
 
         log.info("Deleting a poorly formatted index page")
         open(os.path.join(Projects.FRONTEND, "pages", "index.js"), "w").close()
