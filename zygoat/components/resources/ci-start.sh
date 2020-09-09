ce3e22fa4aeb3b52e19ab1bab98265ca50a732d3
Mark Rawls <markrawls96@gmail.com>
2020-09-09 19:29:12 -0400

    Update the ci-start script for a custom runtime

--- zygoat/components/resources/ci-start.sh
+++ zygoat/components/resources/ci-start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+nohup /usr/local/bin/dockerd --host=unix:///var/run/docker.sock --host=tcp://127.0.0.1:2375 --storage-driver=overlay2&
+
+timeout 15 sh -c "until docker info 2>/dev/null; do echo .; sleep 1; done"
+
 COMPOSE_HTTP_TIMEOUT=200 docker-compose up -d --build
