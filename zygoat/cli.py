15b2e0841f8b1b27aeceb222da96e1be56a9e1bb
Lianne <lianne@lianne.lairmore>
2021-04-26 13:14:18 -0400

    CLI: Don't allow update with older version unless forced

--- zygoat/cli.py
+++ zygoat/cli.py
@@ -1,6 +1,7 @@
 import click
 from importlib import import_module
 import logging
+import semver
 
 from .components import components
 from .config import Config
@@ -9,9 +10,38 @@ from .constants import Phases, config_file_name, __version__
 log = logging.getLogger()
 
 
-def _call_phase(phase, reverse=False):
+class ZygoatOutOfDateException(Exception):
+    """
+    Exception raised when trying to run zygoat on a project initialized with
+    a newer version of zygoat.
+    """
+
+    def __init__(self, current_version, original_version):
+        super().__init__(
+            f"Tried to update a zygoat project using a older "
+            f"version than the project was created with."
+            f"Run with --force if this is intended.\n"
+            f"Original version: {original_version}\n"
+            f"Current version: {current_version}"
+        )
+
+
+def _is_older_version(version1, version2):
+    return semver.compare(version1, version2) < 0
+
+
+def _call_phase(phase, reverse=False, force=False):
     component_list = [*components]
     config = Config()
+
+    version = config.get("version")
+    if _is_older_version(__version__, version):
+        log.warn(
+            f"Using older version of zygoat than when this package was made. "
+            f"Current: {__version__}, Original: {version}."
+        )
+        if not force:
+            raise ZygoatOutOfDateException(__version__, version)
 
     for extra in config.get("extras", []):
         module, attr = extra.split(":")
@@ -54,13 +84,14 @@ def init(project_name):
 
 
 @cli.command(help="Creates a new settings file and all components")
+@click.option("--force", "-f", is_flag=True)
 @click.argument("project_name", default="")
-def new(project_name):
+def new(project_name, force):
     log.debug(f"Attempting creation of {click.style(project_name, bold=True)}")
 
     _name_project(project_name)
 
-    _call_phase(Phases.CREATE)
+    _call_phase(Phases.CREATE, force=force)
 
     log.info("Done!")
     log.info(
@@ -72,21 +103,24 @@ def new(project_name):
 
 
 @cli.command(help="Lists all of the running phase names")
-def list():
-    _call_phase(Phases.LIST)
+@click.option("--force", "-f", is_flag=True)
+def list(force):
+    _call_phase(Phases.LIST, force=force)
 
 
 @cli.command(help="Calls the delete phase on all included build components")
-def delete():
-    _call_phase(Phases.DELETE, reverse=True)
+@click.option("--force", "-f", is_flag=True)
+def delete(force):
+    _call_phase(Phases.DELETE, reverse=True, force=force)
 
     # remove zygoat settings file
     Config.delete()
 
 
 @cli.command(help="Calls the update phase on all included build components")
-def update():
-    _call_phase(Phases.UPDATE)
+@click.option("--force", "-f", is_flag=True)
+def update(force):
+    _call_phase(Phases.UPDATE, force=force)
 
 
 @cli.command(help="Prints the version and exits")
