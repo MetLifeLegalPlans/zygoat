37f599851cb1e41aa9a4b92536548b56376ced72
Mark Rawls <markrawls96@gmail.com>
2021-10-22 18:21:30 -0400

    Remove Willing-specific components

--- zygoat/components/__init__.py
+++ zygoat/components/__init__.py
@@ -4,8 +4,6 @@ from .settings_component import SettingsComponent
 from . import resources
 
 from .editorconfig import editorconfig
-from .codebuild import codebuild
-from .script import cistart
 from .precommit import precommitconfig
 from .docker_compose import docker_compose
 from .backend import backend
@@ -19,8 +17,6 @@ components = [
     backend,
     frontend,
     reverse_proxy,
-    codebuild,
-    cistart,
 ]
 
 __all__ = [
