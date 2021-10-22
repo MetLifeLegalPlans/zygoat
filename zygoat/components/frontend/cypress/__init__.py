58ce4e5cb4a558d268eafc84c107c0266ba12f7b
Mark Rawls <markrawls96@gmail.com>
2021-10-22 18:21:30 -0400

    Remove Willing-specific components

--- zygoat/components/frontend/cypress/__init__.py
+++ zygoat/components/frontend/cypress/__init__.py
@@ -1,6 +1,5 @@
 from zygoat.components import Component
 
-from .codebuild import codebuild
 from .gitignore import gitignore
 from .configuration import configuration
 
@@ -9,4 +8,4 @@ class Cypress(Component):
     pass
 
 
-cypress = Cypress(sub_components=[configuration, gitignore, codebuild])
+cypress = Cypress(sub_components=[configuration, gitignore])
