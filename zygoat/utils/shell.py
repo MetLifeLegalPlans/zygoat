8f365c7a358fe394beefa3435d8d328ce0a8e94a
Mark Rawls <markrawls96@gmail.com>
2021-12-07 18:17:11 -0500

    Use docker to initialize the backend

--- zygoat/utils/shell.py
+++ zygoat/utils/shell.py
@@ -18,13 +18,21 @@ def run(cmd, *args, **kwargs):
     )
 
 
-def docker_run(cmd, image, vol, chown=True):
+def docker_run(cmd, image, vol, chown=True, *args, **kwargs):
     vol_directory = os.path.join(os.getcwd(), vol)
     prelude = ["docker", "run", "-v", f"{vol_directory}:/data", "-w", "/data", image]
-    run(prelude + cmd)
+    ret = run(prelude + cmd, *args, **kwargs)
 
     if chown:
         uid = os.getuid()
         gid = os.getuid()
 
         run(prelude + ["chown", "-R", f"{uid}:{gid}", "/data"])
+
+    return ret
+
+
+def multi_docker_run(cmds, *args, **kwargs):
+    cmd = "".join([shlex.join(c) + ";" for c in cmds])
+    print(cmd)
+    return docker_run(["/bin/bash", "-c", cmd], *args, **kwargs)
