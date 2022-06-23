075c5f12b178a6bbe349378b4181f3a7992cfb96
kelarcrisp <kelcrisp007@icloud.com>
2022-06-23 13:18:54 -0700

    fix linting

--- zygoat/components/frontend/resources/next.config.js
+++ zygoat/components/frontend/resources/next.config.js
@@ -1,9 +1,9 @@
-const withPlugins = require("next-compose-plugins");
+const withPlugins = require('next-compose-plugins');
 
-const { plugins, config } = require("./zygoat.next.config");
+const { plugins, config } = require('./zygoat.next.config');
 
 const contentSecurityPolicy = {
-  key: "Content-Security-Policy",
+  key: 'Content-Security-Policy',
   value:
     "default-src 'self' https://static.zdassets.com/ekr/snippet.js https://www.google-analytics.com/analytics.js https://cdnjs.cloudflare.com https://hello.myfonts.net https://ekr.zendesk.com https://widget-mediator.zopim.com https://willing.zendesk.com https://www.google-analytics.com https://v2assets.zopim.io http://hello.myfonts.net/count/36e46e https://static.zdassets.com 'unsafe-eval' 'unsafe-inline'",
 };
