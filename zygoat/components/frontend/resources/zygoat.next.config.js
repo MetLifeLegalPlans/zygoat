8eb7a6de5094bca2a0ebf5eef86ed8f87ada4aee
kelarcrisp <kelcrisp007@icloud.com>
2022-08-10 13:51:53 -0700

    add react strict mode to next config

--- zygoat/components/frontend/resources/zygoat.next.config.js
+++ zygoat/components/frontend/resources/zygoat.next.config.js
@@ -2,18 +2,21 @@
 // be edited manually. To extend or overwrite these settings, edit
 // next.config.js
 
-const withSvgr = require('next-svgr');
-const withImages = require('next-images');
+const withSvgr = require("next-svgr");
+const withImages = require("next-images");
 
-const prod = process.env.NODE_ENV === 'production';
+const prod = process.env.NODE_ENV === "production";
 
 const headers = [
-  { key: 'X-FRAME-Options', value: 'DENY' },
-  { key: 'Content-Security-Policy', value: "frame-ancestors 'none'" },
-  { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
-  { key: 'Cache-Control', value: 'no-cache, no-store' },
-  { key: 'X-Content-Type-Options', value: 'nosniff' },
-  { key: 'X-XSS-Protection', value: '1; mode=block' },
+  { key: "X-FRAME-Options", value: "DENY" },
+  { key: "Content-Security-Policy", value: "frame-ancestors 'none'" },
+  {
+    key: "Strict-Transport-Security",
+    value: "max-age=31536000; includeSubDomains",
+  },
+  { key: "Cache-Control", value: "no-cache, no-store" },
+  { key: "X-Content-Type-Options", value: "nosniff" },
+  { key: "X-XSS-Protection", value: "1; mode=block" },
 ];
 
 const buildHeaders =
@@ -21,13 +24,13 @@ const buildHeaders =
   async () =>
     [
       {
-        source: '/:path*',
+        source: "/:path*",
         // This file does not get compiled
         // eslint-disable-next-line object-shorthand
         headers: [...headers, ...overrideHeaders],
       },
       {
-        source: '/',
+        source: "/",
         // eslint-disable-next-line object-shorthand
         headers: [...headers, ...overrideHeaders],
       },
@@ -36,13 +39,13 @@ const buildHeaders =
 const config = {
   webpack5: true,
   webpack: (webpackConfig, { webpack }) => {
-    webpackConfig.resolve.alias['@@'] = __dirname;
-    webpackConfig.resolve.alias['@wui'] = '@bequestinc/wui';
+    webpackConfig.resolve.alias["@@"] = __dirname;
+    webpackConfig.resolve.alias["@wui"] = "@bequestinc/wui";
 
     webpackConfig.plugins.push(
       new webpack.ProvidePlugin({
-        React: 'react',
-      }),
+        React: "react",
+      })
     );
 
     return webpackConfig;
@@ -55,6 +58,7 @@ const config = {
   productionBrowserSourceMaps: true,
   poweredByHeader: false,
   swcMinify: true,
+  reactStrictMode: true,
 };
 
 const withImagesConfig = {
