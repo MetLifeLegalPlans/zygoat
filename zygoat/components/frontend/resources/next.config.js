d060ce3512aaa20929cb2e3149b09b545aa424dc
Kris Borer <kris@willing.com>
2023-04-18 13:36:33 -0400

    HEADERS: add frame-ancestors policy directive

--- zygoat/components/frontend/resources/next.config.js
+++ zygoat/components/frontend/resources/next.config.js
@@ -5,7 +5,7 @@ const { plugins, config } = require('./zygoat.next.config');
 const contentSecurityPolicy = {
   key: 'Content-Security-Policy',
   value:
-    "default-src 'self' https://static.zdassets.com/ekr/snippet.js https://www.google-analytics.com/analytics.js https://cdnjs.cloudflare.com https://hello.myfonts.net https://ekr.zendesk.com https://widget-mediator.zopim.com https://willing.zendesk.com https://www.google-analytics.com https://v2assets.zopim.io http://hello.myfonts.net/count/36e46e https://static.zdassets.com 'unsafe-eval' 'unsafe-inline'; upgrade-insecure-requests",
+    "default-src 'self' https://static.zdassets.com/ekr/snippet.js https://www.google-analytics.com/analytics.js https://cdnjs.cloudflare.com https://hello.myfonts.net https://ekr.zendesk.com https://widget-mediator.zopim.com https://willing.zendesk.com https://www.google-analytics.com https://v2assets.zopim.io http://hello.myfonts.net/count/36e46e https://static.zdassets.com 'unsafe-eval' 'unsafe-inline'; frame-ancestors 'none'; upgrade-insecure-requests",
 };
 
 const extendedConfig = {
