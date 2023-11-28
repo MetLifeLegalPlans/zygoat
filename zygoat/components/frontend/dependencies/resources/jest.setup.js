d73807a1063b254c460faca2c8f5d4844fc829c1
Aria Taylor <ari@willing.com>
2023-11-28 11:10:09 -0500

    Update jest imports

--- zygoat/components/frontend/dependencies/resources/jest.setup.js
+++ zygoat/components/frontend/dependencies/resources/jest.setup.js
@@ -1,4 +1,5 @@
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/jest-globals';
 import mediaQuery from 'css-mediaquery';
 import * as nextRouter from 'next/router';
 
